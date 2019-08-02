7f9789cc0aa3d3e71db1abd63ac2f84e10086502
George Philbrick <gphilbri@gmail.com>
2019-08-02 18:02:46 -0400

    Basic Setup

---
+++ Game Jam Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player1;
+    public Transform player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPosition = (player1.position + player2.position) / 2;
+        cameraPosition.z = -10;
+        transform.position = cameraPosition;
+    }
+}

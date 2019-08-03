606e24e53c77c96ad3be16d09eb331f925db2814
George Philbrick <gphilbri@gmail.com>
2019-08-03 12:37:50 -0400

    Button Platform

---
+++ Game Jam Project/Assets/Scripts/ButtonPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPlatform : MonoBehaviour
+{
+    public GameObject platform;
+    public Sprite buttonDownSprite;
+    Sprite buttonUpSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform.GetComponent<SpriteRenderer>().color = new Color (1f, 1f, 1f, 0.3f);
+        platform.GetComponent<BoxCollider2D>().enabled = false;
+        buttonUpSprite = GetComponent<SpriteRenderer>().sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" || other.tag == "Ground")
+        {
+            GetComponent<SpriteRenderer>().sprite = buttonDownSprite;
+            platform.GetComponent<SpriteRenderer>().color = Color.white;
+            platform.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player" || other.tag == "Ground")
+        {
+            GetComponent<SpriteRenderer>().sprite = buttonUpSprite;
+            platform.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.3f);
+            platform.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}

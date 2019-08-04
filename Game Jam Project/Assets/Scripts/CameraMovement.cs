00601baab7ac8107393f4d7eeaf9f6f89fcdf68d
George Philbrick <gphilbri@gmail.com>
2019-08-03 23:39:20 -0400

    Camera

--- Game Jam Project/Assets/Scripts/CameraMovement.cs
+++ Game Jam Project/Assets/Scripts/CameraMovement.cs
@@ -7,18 +7,27 @@ public class CameraMovement : MonoBehaviour
     Transform player1,
         player2;
 
+    public float zoomFactor = 1;
+    float initialSize;
+
     // Start is called before the first frame update
     void Start()
     {
          player1 = GameManager.instance.player1.GetComponent<Transform>();
          player2 = GameManager.instance.player2.GetComponent<Transform>();
+         initialSize = GetComponent<Camera>().orthographicSize;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 playerDistance = player1.position - player2.position;
         Vector3 cameraPosition = (player1.position + player2.position) / 2;
         cameraPosition.z = -10;
         transform.position = cameraPosition;
+        if (playerDistance.magnitude > initialSize)
+        {
+            GetComponent<Camera>().orthographicSize = zoomFactor * playerDistance.magnitude;
+        }
     }
 }
